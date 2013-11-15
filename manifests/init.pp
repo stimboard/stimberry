f3b17c1a9c39964077a911ac28aeefe60d889256
root <root@raspberrypi.(none)>
2013-11-15 10:17:53 +0000

    clone git added

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,17 @@ class stimberry {
 			provider => 'npm',;
 	}
 
+	file { ['/var/www', '/var/www/nodesites']:
+		ensure  => 'directory',
+	}
+	vcsrepo { '/var/www/nodesites/trivial-chat':
+		ensure   => present,
+		provider => git,
+		source   => 'git://github.com/fmaurica/trivial-chat.git',
+
+		require  => File['/var/www/nodesites'],
+	}
+
 	class { 'fm_hostapd':
 		interface  => 'wlan0',
 		ssid       => 'stimberry_wn',
