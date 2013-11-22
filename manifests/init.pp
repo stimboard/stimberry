4457ce423a4b0e54ac80a942a81bef62247e7eb0
root <root@raspberrypi.(none)>
2013-11-22 12:46:36 +0000

    ui cloned

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,9 +4,10 @@ class stimberry {
 		['nodejs', 'npm']:,;
 	}
 	package {['mysql-server']: }
-#	service { 'mysql-server':
-#		ensure     => 'running',
-#	}
+	service { 'mysql':
+		ensure     => 'running',
+	}
+	package {['iceweasel']:}
 
 	file { '/usr/bin/node':
 		ensure  => 'link',
@@ -22,6 +23,16 @@ class stimberry {
 		source   => 'git://github.com/laza1618/T8_node_server_projet.git',
 
 		require  => File['/var/www/nodesites'],
+	}
+	vcsrepo { '/var/www/nodesites/stimboard/ui':
+		ensure   => present,
+		provider => git,
+		source   => 'git://github.com/Fenitra/STIMBoard.git',
+
+		require  => [
+			File['/var/www/nodesites'],
+			Vcsrepo['/var/www/nodesites/stimboard'],
+			    ],
 	}
 	# node_modules has to be done manually because not supported by puppetlabs...
 	service { 'stimboard':
